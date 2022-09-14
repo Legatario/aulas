73c3be4802c057940f2818292146f8af3ee6b9d2
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-14 15:21:03 -0300

    arrumando bugs por causa da ,

--- src/Components/IMC/Imc.js
+++ src/Components/IMC/Imc.js
@@ -1,5 +1,5 @@
 import './style.css'
-import {useState} from 'react'
+import React, {useState} from 'react'
 
 function Imc(){
     const [weight, setWeight] = useState() 
@@ -7,30 +7,49 @@ function Imc(){
     const [imc, setImc] = useState()
 
     function calcular(){
+         
+        setWeight(parseFloat(weight).toFixed(2))
+        setHeight(parseFloat(height).toFixed(2))
         setImc(parseFloat(weight/(height*height)))
     }
     return(
         <div className='IMC'>
             <h2> Cálculo de IMC</h2>
             <div className='field'>
-            <input type='text' 
+            <input type='number' 
             placeholder="digite seu peso"
-            step="0.01" 
+            step="0.01"
+            required 
             value={weight} 
-            onChange={e => setWeight(e.target.value)}
+            onChange={(e) => setWeight(+e.target.value)}
             />
 
-            <input type='text' 
+            <input type='number' 
             placeholder="digite sua altura" 
             value={height} 
             step="0.01"
-            onChange={e => setHeight(e.target.value)}
+            required
+            onChange={e => setHeight(+e.target.value)}
             />
             <button onClick={calcular}>Calcular</button>
             </div>
+            {imc > 0 && imc !== Infinity &&(
             <p>
             Resultado: {imc}
             </p>
+            )}
+            {imc === Infinity &&(
+            <p>
+                O seu peso não pode ser 0
+            </p>
+            )
+            }
+            {!imc &&(
+            <p>
+                Preencha os campos acima
+            </p>
+            )
+            }
         </div>
     )
 
