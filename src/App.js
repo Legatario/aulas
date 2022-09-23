147c3f537be98ead504e792b7d693507d372126f
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-23 13:12:53 -0300

    criando login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Imc from './Components/IMC/Imc';
+import Login from './Components/Login/Login'
 
 function App() {
   return (
     <div className="App">
       <Header title="Título do APP"/>
       <Imc />
+
+      <Login isLoggedIn={false} name={'Luis'}/>
     </div>
   );
 }
