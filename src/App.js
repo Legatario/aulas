61d65a863c49889678e66464889247bc54cc33af
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-27 16:05:06 -0300

    ui integração

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Imc from './Components/IMC/Imc';
 import Login from './Components/Login/LoginControl';
-import { Heading1, Loader } from './Components/Styles/styles'
+import { Heading1, Loader } from './Components/Styles/styles';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      <Heading1>
-        Styles
-        <span>Subtitulo</span>
-      </Heading1>
-      <Header title="Título do APP"/>
-      <Imc />
 
-      <Login />
-      
-      <Loader />
+    <Heading1> 
+      Styles 
+      <span>Subtitulo</span> 
+    </Heading1>
+
+    <Header title="Título do APP"/>
+    <Imc />
+    
+    <Login />
+    
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+    
+    <Loader />
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,14 @@
 import {useState} from 'react'
-import logo from './../../logo.svg'
+import logo from './../../logo.svg';
+import * as React from 'react';
+import Switch from '@mui/material/Switch';
 
 
 function Header(props){
 
     const [count, setCount] = useState(1);
+
+    const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
     function iCount(){
         setCount(count+1);
@@ -17,6 +21,9 @@ function Header(props){
             <img src={logo} alt="logo" className='logo'/>
             <p>Bem vindo! Pela {count} vez!!!</p>
             <button onClick={iCount}>retornar</button>
+            <div>
+                <Switch {...label} />
+            </div>
         </header>
     )
 }
