3df0d1c27b76258d1aa5d809b5e1c69fc0c10cb1
Luis Phillipe <l.phillipe.d@gmail.com>
2022-09-27 12:32:28 -0300

    criando styles component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Imc from './Components/IMC/Imc';
-import Login from './Components/Login/LoginControl'
+import Login from './Components/Login/LoginControl';
+import { Heading1, Loader } from './Components/Styles/styles'
 
 function App() {
   return (
     <div className="App">
+      <Heading1>
+        Styles
+        <span>Subtitulo</span>
+      </Heading1>
       <Header title="Título do APP"/>
       <Imc />
 
       <Login />
+      
+      <Loader />
     </div>
   );
 }
